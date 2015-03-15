45035a4f6861f05005e119adc89b8ef2bf9c76c1
kimkc <kimkc@192.168.1.73>
2015-03-15 17:39:25 +0900

    ..

--- staff/src/main/java/com/staff/dao/MakerDao.java
+++
@@ -1,9 +0,0 @@
-package com.staff.dao;
-
-import java.util.List;
-
-import com.staff.domain.Maker;
-
-public interface MakerDao {
-  List<Maker> selectNameList();
-}
--- staff/src/main/java/com/staff/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.staff.dao;
-
-import java.util.Map;
-
-import com.staff.domain.Member;
-
-public interface MemberDao {
-  Member existUser(Map<String,String> params);
-}
